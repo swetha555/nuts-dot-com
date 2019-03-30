6c754a7566a798219f3e08d0a3c9c3258df41462
swetha vengala <swetha@swethas-MacBook-Pro.local>
2019-03-30 11:01:44 -0400

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ProductListComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
